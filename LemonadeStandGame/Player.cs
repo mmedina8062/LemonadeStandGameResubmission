0827259d52b9eb69fb240b8c3bcd874d8d6e31a6
unknown <mmedina8062@gmail.com>
2019-10-12 11:01:52 -0500

    added variables Wallet & Inventory in Player class & added Methods in Player class; SetIngredients allows player to choose how much, SetPrice allows player to set price according to weather, WalletTransaction allows money to be subtracted and added, & AdjustInventory allows ingredients to be subtractedor added

--- LemonadeStandGame/Player.cs
+++ LemonadeStandGame/Player.cs
@@ -7,12 +7,35 @@ namespace LemonadeStandGame
 {
     public class Player
     {
+        public int Wallet;
+        public int Inventory;
+
         public Game Game
         {
             get => default;
             set
             {
             }
+        }
+
+        public void SetIngredients()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetPrice()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WalletTransactions()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AdjustInventory()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
