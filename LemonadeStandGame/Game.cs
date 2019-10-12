0461a56b7ee00e5bf0ee19ca83757e8118fa2019
unknown <mmedina8062@gmail.com>
2019-10-12 10:46:37 -0500

    added memeber variables public store, player, day, userInterface in Game class, and instantiated in contructor in Game class

--- LemonadeStandGame/Game.cs
+++ LemonadeStandGame/Game.cs
@@ -7,5 +7,23 @@ namespace LemonadeStandGame
 {
     public class Game
     {
+        //member variables
+        public Store store;
+        public Player player;
+        public Day day;
+        public UserInterface userInterface;
+
+
+        //ctor
+        public Game()
+        {
+            Store store = new Store();
+            Player player = new Player();
+            Day day = new Day();
+            UserInterface userInterface = new UserInterface();
+        }
+
+
+        //member methods
     }
 }
